82e78620e6c17e37ea2a97d0444dea80f8305389
h. mikail <>
2023-07-24 06:57:25 -0700

    bmi sol

--- challenge.js
+++ challenge.js
@@ -1,2 +1,25 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+function calculateBMI(weight, height) {
+    return weight / (height * height)
+  }
+  
+  
+  const aliWeight = 75 
+  const aliHeight = 1.8
+  
+  const mohamedWeight = 80
+  const mohamedHeight = 1.75
+  
+  const aliBMI = calculateBMI(aliWeight, aliHeight)
+  const mohamedBMI = calculateBMI(mohamedWeight, mohamedHeight)
+  
+  console.log("Ali's BMI:", aliBMI)
+  console.log("Mohamed's BMI:", mohamedBMI)
+  
+  
+  if (aliBMI > mohamedBMI) {
+    console.log("Ali has a higher BMI than Mohamed.")
+  } else if (mohamedBMI > aliBMI) {
+    console.log("Mohamed has a higher BMI than Ali.")
+  } else {
+    console.log("Ali and Mohamed have the same BMI.")
+  }
